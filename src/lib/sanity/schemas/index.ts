d049c52f0c9b70c90fcdc8d159b01b5a5f566c64
umutcantezgel-cpu <umut@codayweb.de>
2026-04-16 07:34:08 -0700

    feat(api): store reservations in sanity database

--- src/lib/sanity/schemas/index.ts
+++ src/lib/sanity/schemas/index.ts
@@ -3,7 +3,8 @@ import allergen from './allergen'
 import category from './category'
 import dish from './dish'
 import siteSettings from './siteSettings'
+import reservation from './reservation'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [siteSettings, allergen, category, dish],
+  types: [siteSettings, allergen, category, dish, reservation],
 }
